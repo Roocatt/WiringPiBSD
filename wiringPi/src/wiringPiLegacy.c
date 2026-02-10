dcb8d524922875e9dfd300ce31e341ee1f9a62b5
Roos Catling-Tate <roos@catling-tate.net>
2026-02-10 12:11:08 -0500

    Reorganize and refactor a bit

---
+++ wiringPi/src/wiringPiLegacy.c
@@ -0,0 +1,60 @@
+
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "wiringPi.h"
+
+#include "wiringPiLegacy.h"
+
+extern int wiringPiDebug;
+
+const char *
+get_pi_revision_legacy(char *line, int linelength, unsigned int *revision)
+{
+	FILE *cpuFd;
+	char *c = NULL;
+
+	if ((cpuFd = fopen("/proc/cpuinfo", "r")) == NULL)
+		pi_gpio_layout_oops("Unable to open /proc/cpuinfo");
+
+	while (fgets(line, linelength, cpuFd) != NULL)
+		if (strncmp(line, "Revision", 8) == 0)
+			break;
+
+	fclose(cpuFd);
+
+	if (strncmp(line, "Revision", 8) != 0)
+		pi_gpio_layout_oops("No \"Revision\" line");
+
+	// Chomp trailing CR/NL
+
+	for (c = &line[strlen(line) - 1]; (*c == '\n') || (*c == '\r'); --c)
+		*c = 0;
+
+	if (wiringPiDebug)
+		printf("get_pi_revision_legacy: Revision string: %s\n", line);
+
+	// Need to work out if it's using the new or old encoding scheme:
+
+	// Scan to the first character of the revision number
+
+	for (c = line; *c; ++c)
+		if (*c == ':')
+			break;
+
+	if (*c != ':')
+		pi_gpio_layout_oops("Bogus \"Revision\" line (no colon)");
+
+	// Chomp spaces
+
+	++c;
+	while (isspace(*c))
+		++c;
+
+	if (!isxdigit(*c))
+		pi_gpio_layout_oops("Bogus \"Revision\" line (no hex digit at start of revision)");
+
+	*revision = (unsigned int) strtol(c, NULL, 16); // Hex number with no leading 0x
+	return (c);
+}
--- wiringPi/wiringPiLegacy.h
+++
@@ -1,7 +0,0 @@
-
-#ifndef __WIRINGPI_LEGACY_H__
-#define __WIRINGPI_LEGACY_H__
-
-const char *get_pi_revision_legacy(char *, int, unsigned int *);
-
-#endif /* __WIRINGPI_LEGACY_H__ */
